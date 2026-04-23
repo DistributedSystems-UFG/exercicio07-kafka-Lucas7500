c2a1c3e5317c86546c519aa9c7f2204a4236e1f4
Lucas Iglesias <119307150+Lucas7500@users.noreply.github.com>
2026-04-23 00:21:26 +0000

    initial commit

--- consumer.py
+++ consumer.py
@@ -1,12 +1,9 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 from const import *
 import sys
 
-# Create consumer: Option 1 -- only consume new events
 consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
-
-# Create consumer: Option 2 -- consume old events (uncomment to test -- and comment Option 1 above)
-#consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
+producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 
 try:
   topic = sys.argv[1]
@@ -16,4 +13,7 @@ except:
   
 consumer.subscribe([topic])
 for msg in consumer:
-    print (msg.value)
+    msgStr = str(msg.value)
+    print(msgStr)
+    producer.send(topic + '_reply', value=msgStr.encode())
+    producer.flush()
